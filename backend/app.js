acc3820f5cce8526e9cab9582c16af24dd6d7487
hamzaadil56 <hamzaadil56@gmail.com>
2022-06-01 09:02:07 +0500

    mern deploy

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,6 @@ const path = require("path");
 app.use(express.json());
 app.use(cookieParser());
 app.use(fileUpload());
-// Route Imports
 const productRouter = require("./routes/productRoutes");
 app.use("/api/v1", productRouter);
 const userRouter = require("./routes/userRoute");
@@ -17,15 +16,6 @@ const orderRouter = require("./routes/orderRoutes");
 app.use("/api/v1", orderRouter);
 const paymentRouter = require("./routes/paymentRoutes");
 app.use("/api/v1", paymentRouter);
-
-// const loginRouter = require("./routes/userRoute");
-// app.use(("/api/v1", loginRouter));
-
-// const logoutRouter = require("./routes/userRoute");
-// app.use(("api/v1", logoutRouter));
-
-// const forgotPasswordRouter = require("./routes/userRoute");
-// app.use(("api/v1", forgotPasswordRouter));
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
